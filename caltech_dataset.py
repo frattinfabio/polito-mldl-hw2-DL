9eb27c45c9517c971d0cc2c0a751ed4bd3d9873f
Fabio Frattin <49120896+frattinfabio@users.noreply.github.com>
2020-06-07 12:50:56 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -1,7 +1,5 @@
 from torchvision.datasets import VisionDataset
-
 from PIL import Image
-
 import os
 import os.path
 import sys
@@ -35,24 +33,7 @@ class Caltech(VisionDataset):
             self.images_paths.append(image_path)
             self.labels.append(self.categories.index(category))
 
-        '''
-        - Here you should implement the logic for reading the splits files and accessing elements
-        - If the RAM size allows it, it is faster to store all data in memory
-        - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the __getitem__ method to access the image-label pair
-          through the index
-        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
-        '''
-
     def __getitem__(self, index):
-        '''
-        __getitem__ should access an element through its index
-        Args:
-            index (int): Index
-
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        '''
         image_path = self.images_paths[index]
         image, label = pil_loader(image_path), label[index]
 
@@ -63,9 +44,5 @@ class Caltech(VisionDataset):
         return image, label
 
     def __len__(self):
-        '''
-        The __len__ method returns the length of the dataset
-        It is mandatory, as this is used by several other components
-        '''
         length = len(self.images_paths) # Provide a way to get the length (number of elements) of the dataset
         return length
